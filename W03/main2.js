4e09b87025504da9627b7b8004ca73f6ddfae66c
takio135 <karasawa931@gmail.com>
2020-05-19 15:34:34 +0900

    Initial commit

--- W03/main2.js
+++ W03/main2.js
@@ -21,7 +21,7 @@ function main()
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
