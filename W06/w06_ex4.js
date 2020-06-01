0b1259b9106cda13986667cdb45dc0744b9f4444
takio135 <karasawa931@gmail.com>
2020-06-01 21:49:09 +0900

    Initial commit

--- W06/w06_ex4.js
+++ W06/w06_ex4.js
@@ -42,9 +42,9 @@ function main()
     for ( var i = 0; i < 256; i++ )
     {
         var S = i / 255.0; // [0,1]
-        var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
-        var G = Math.max( Math.cos( ( S - 0.5 ) * Math.PI ), 0.0 );
-        var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
+        var R = 1;
+        var G = 1-(i /255.0);
+        var B = 1-(i /255.0);
         var color = new THREE.Color( R, G, B );
         cmap.push( [ S, '0x' + color.getHexString() ] );
     }
