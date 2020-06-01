1c3b416246768304aed7a144f42a376e41fda300
takio135 <karasawa931@gmail.com>
2020-06-01 18:51:03 +0900

    Initial commit

--- W05/w05_ex1.js
+++ W05/w05_ex1.js
@@ -22,7 +22,15 @@ function main()
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
-    var material = new THREE.MeshLambertMaterial();
+    var material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('gourand.vert').text,
+        fragmentShader: document.getElementById('gourand.frag').text,
+        uniforms: {
+               light_position: { type: 'v3', value: light.position }
+            }
+    
+    });
 
     var torus_knot = new THREE.Mesh( geometry, material );
     scene.add( torus_knot );
